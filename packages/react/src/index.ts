4b79516310ee14033d3d1f937fdd4e4915d29da1
Victor Hristov <victor.hristov@mentormate.com>
2023-09-29 16:33:07 +0300

    Use `margin` component in playground

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,18 +4,20 @@ import CircleIcon from "./atoms/CircleIcon";
 import Image from "./atoms/Image";
 import Subtitle from "./atoms/Subtitle";
 import Title from "./atoms/Title";
+import Margin from "./atoms/Margin";
 import CardFooter from "./molecules/CardFooter";
 import CardHeader from "./molecules/CardHeader";
 import CardWithOverline from "./organism/CardWithOverline";
 
 export {
-  Button,
-  CTA,
-  Title,
-  CircleIcon,
-  Subtitle,
-  Image,
-  CardHeader,
-  CardFooter,
-  CardWithOverline,
+	Button,
+	CTA,
+	Title,
+	Margin,
+	CircleIcon,
+	Subtitle,
+	Image,
+	CardHeader,
+	CardFooter,
+	CardWithOverline,
 };
--- playgrounds/react/src/App.tsx
+++ playgrounds/react/src/App.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { Button, CTA, CardWithOverline } from "@ds.e/react";
+import { Button, CTA, CardWithOverline, Margin } from "@ds.e/react";
 import "@ds.e/scss/lib/global.css";
 import "@ds.e/scss/lib/utilities.css";
 import "@ds.e/scss/lib/Button.css";
+import "@ds.e/scss/lib/Margin.css";
 import "@ds.e/scss/lib/CTA.css";
 import "@ds.e/scss/lib/Title";
 import "@ds.e/scss/lib/Subtitle";
 import "@ds.e/scss/lib/CircleIcon";
 
 const App = () => {
-  return (
-    <div>
-      <Button label="Click me!" />
-      <CTA className="cta-button">CTA button</CTA>
-      <CardWithOverline
-        cardImg="asd.com"
-        description="Some description"
-        iconSrc="asd.com"
-        subtitle="overline"
-        title="Headline 6"
-      />
-    </div>
-  );
+	return (
+		<div>
+			<Button label="Click me!" />
+			<CTA className="cta-button">CTA button</CTA>
+			<Margin space="sm">
+				<CardWithOverline
+					cardImg="https://upload.wikimedia.org/wikipedia/commons/6/6c/2019_Ford_Mustang_GT_Blue.jpg"
+					description="Some description"
+					iconSrc="asd.com"
+					subtitle="overline"
+					title="Headline 6"
+				/>
+			</Margin>
+		</div>
+	);
 };
 
 export default App;
